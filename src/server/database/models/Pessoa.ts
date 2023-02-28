539fe004b619b82d0c5fcc584413cf8201d89f02
Fabio Luiz Rodrigues <fabioluizrodrigues@gmail.com>
2023-02-28 11:34:10 -0400

    feat: model e migration de pessoas

---
+++ src/server/database/models/Pessoa.ts
@@ -0,0 +1,16 @@
+export interface IPessoa {
+  id: number;
+  cnpj_cpf: string;
+  nome_razao: string;
+  email: string;
+  telefone: string;
+  ie_rg: string;
+  cep: string;
+  estado: string;
+  cidade_id: number;
+  bairro: string;
+  endereco: string;
+  numero: string;
+  complemento: string;
+  observacoes: string;
+}
